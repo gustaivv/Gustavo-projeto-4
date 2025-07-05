b7604a0840faebdb31d4980112c0c991aabe4ddd
gustaivv <gustavo.silva.oliveira23@escola.pr.gov.br>
2025-07-05 19:37:30 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Historia',
+    'Quem descobriu o brasil',
+    'Pedro Alvares Cabral'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasilia'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual é o maior Jogador Dá historia?',
+    'Pelé'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz dia em Inglês?',
+    'Day'
+)
